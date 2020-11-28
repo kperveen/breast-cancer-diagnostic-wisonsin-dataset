6da25b991e6cd589a0f411dec651802bed84f4a7
meharshahani <45812963+meharshahani@users.noreply.github.com>
2020-11-28 13:56:52 -0600

    Update load_data_to_dir.sh

--- load_data_to_dir.sh
+++ load_data_to_dir.sh
@@ -1,8 +1,8 @@
 # create a directory for project
 hdfs dfs -mkdir /breast-cancer-analysis
 
-#scp the data.csv file to the directory created in the previous step
-scp -i <path_to_emr_key_pair> data.csv hadoop@ec2-0-0-0-0.compute-1.amazon.com:/home/hadoop/breast-cancer-analysis
+#scp the data.csv file to the home directory
+scp -i <path_to_emr_key_pair> data.csv hadoop@ec2-0-0-0-0.compute-1.amazon.com:/home/hadoop/
 
-# Import dataset
+# Import dataset to the directory
 hadoop fs -put data.csv /breast-cancer-analysis/
