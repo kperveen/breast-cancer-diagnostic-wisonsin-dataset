8b80ab0d853404f82efbc967e2886c26537be8dd
meharshahani <45812963+meharshahani@users.noreply.github.com>
2020-11-29 14:04:20 -0600

    Create ML-model.py

---
+++ ML-model.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+dataset = pd.read_csv('/Users/meharshahani/Downloads/data.csv')
+X = dataset.iloc[:, 1:31].values
+Y = dataset.iloc[:, 31].values
+
+dataset.head()
+print("Cancer data set dimensions : {}".format(dataset.shape))
